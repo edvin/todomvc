60e8b79d26541e593da028c7c15eaf406ca4982f
Edvin Syse <es@syse.no>
2017-03-19 16:19:17 +0100

    Use items.count instead of items.filter -> count

--- src/main/kotlin/todomvcfx/tornadofx/views/Footer.kt
+++ src/main/kotlin/todomvcfx/tornadofx/views/Footer.kt
@@ -7,7 +7,7 @@ import tornadofx.*
 
 class Footer : View() {
     val store: Store by inject()
-    val itemsLeft = integerBinding(store.todos) { items.filter { !it.completed }.count() }
+    val itemsLeft = integerBinding(store.todos) { items.count { !it.completed } }
 
     override val root = hbox {
         addClass(Styles.footer)
